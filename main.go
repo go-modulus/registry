1172dd1b1c8d35b1add1ca8555dba4fd10d2b410
Andrii Yanakov <yanak1984@gmail.com>
2026-03-16 17:43:46 +0200

    Update registry of modules with new places of moved modules

--- main.go
+++ main.go
@@ -1,84 +1,62 @@
 package main
 
 import (
+	"cmp"
 	"fmt"
+	"slices"
 
 	"github.com/fatih/color"
-	"github.com/go-modulus/modulus/auth"
-	"github.com/go-modulus/modulus/auth/providers/email"
-	"github.com/go-modulus/modulus/auth/providers/google"
+	"github.com/go-modulus/auth"
+	"github.com/go-modulus/auth/providers/email"
+	"github.com/go-modulus/auth/providers/google"
+	"github.com/go-modulus/chihttp"
+	"github.com/go-modulus/graphql"
 	"github.com/go-modulus/modulus/captcha"
 	"github.com/go-modulus/modulus/cli"
-	"github.com/go-modulus/modulus/db/embedded"
-	"github.com/go-modulus/modulus/db/migrator"
-	"github.com/go-modulus/modulus/db/pgx"
-	"github.com/go-modulus/modulus/graphql"
 	"github.com/go-modulus/modulus/http"
-	httpMiddleware "github.com/go-modulus/modulus/http/middleware"
 	"github.com/go-modulus/modulus/logger"
 	"github.com/go-modulus/modulus/module"
-	"github.com/go-modulus/modulus/temporal"
 	"github.com/go-modulus/modulus/translation"
+	"github.com/go-modulus/pgx"
+	"github.com/go-modulus/pgx/migrator"
 	"github.com/go-modulus/registry/internal"
+	"github.com/go-modulus/temporal"
 
 	"os"
 )
 
 func main() {
 	modules := []module.Manifesto{
-		module.NewManifesto(
-			cli.NewModule(),
-			"github.com/go-modulus/modulus/cli",
-			"Adds ability to create cli applications in the Modulus framework.",
-			"1.0.0",
-		),
+		cli.NewManifesto(),
 		pgx.NewManifesto(),
-		module.NewManifesto(
-			logger.NewModule(),
-			"github.com/go-modulus/modulus/logger",
-			"Adds a slog logger with a zap backend to the Modulus framework.",
-			"1.0.0",
-		),
-		module.NewManifesto(
-			migrator.NewModule(),
-			"github.com/go-modulus/modulus/db/migrator",
-			"Several CLI commands to use DBMate (https://github.com/amacneil/dbmate) migration tool inside your application.",
-			"1.0.0",
-		),
-		module.NewManifesto(
-			http.NewModule(),
-			"github.com/go-modulus/modulus/http",
-			"HTTP module based on the Chi router.",
-			"1.0.0",
-		),
-		module.NewManifesto(
-			httpMiddleware.NewModule(),
-			"github.com/go-modulus/modulus/http/middleware",
-			"Various useful middlewares",
-			"1.0.0",
-		),
+		migrator.NewManifesto(),
+		logger.NewManifesto(),
+		http.NewManifesto(),
+		chihttp.NewManifesto(),
 		graphql.NewManifesto(),
 		auth.NewManifesto(),
 		email.NewManifesto(),
-		embedded.NewManifesto(),
 		temporal.NewManifesto(),
 		captcha.NewManifesto(),
 		google.NewManifesto(),
 		translation.NewManifesto(),
 	}
 
-	registry, err := internal.LoadLocalRegistry("./")
-	if err != nil {
-		fmt.Println("Cannot load the registry file modules.json:", color.RedString(err.Error()))
-		os.Exit(1)
+	registry := internal.Registry{
+		Modules:     make([]module.Manifesto, 0),
+		Version:     "1.0.0",
+		Name:        "Modulus framework modules registry",
+		Description: "List of available modules for the Modulus framework",
 	}
+
+	slices.SortFunc(modules, func(a, b module.Manifesto) int { return cmp.Compare(a.Name, b.Name) })
 
 	for _, currentModule := range modules {
 		fmt.Println("Updating module", color.BlueString(currentModule.Name))
 		registry.UpdateModule(currentModule)
 	}
 
-	err = registry.SaveAsLocalFile("./")
+	err := registry.SaveAsLocalFile("./")
 	if err != nil {
 		fmt.Println("Cannot save the registry file modules.json:", color.RedString(err.Error()))
 		os.Exit(1)
